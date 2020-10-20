a569b14a82ce51932a0773e22f2957602801ced2
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-10-20 10:25:58 +0200

    FZWSConfig added

---
+++ FZWS.Core/FZWSClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FZWS.Core
+{
+    public class FZWSClient
+    {
+        private const string URL_SIFARNICI = "https://trust1.fzocg.net:443/wspaZalihe/getSifarnici";
+        private const string URL_LAGER = "https://trust1.fzocg.net:443/wspaZalihe/prijemLagera";
+
+        public FZWSClient()
+        {
+
+        }
+
+    }
+}
---
+++ FZWS.Core/FZWSConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FZWS.Core
+{
+    public class FZWSConfig
+    {
+        public string SifraUstanove { get; set; }
+        public string OrgJedinicaId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string LagerUrl { get; set; }
+        public string SifarniciUrl { get; set; }
+
+        public void AddAuthHeader(HttpWebRequest request)
+        {
+            String encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(Username + ":" + Password));
+            request.Headers.Add("Authorization", "Basic " + encoded);
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+    }
+}
